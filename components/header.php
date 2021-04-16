496b20e3f9e5d2546b82ede06a76b98d720947f2
Chris <chrisliu0416@gmail.com>
2021-04-16 12:18:10 -0700

    first commit

---
+++ components/header.php
@@ -0,0 +1,9 @@
+<header class="masthead">
+    <div class="container d-flex h-100 align-items-center">
+        <div class="mx-auto text-center">
+            <h1 class="mx-auto my-0 text-uppercase">BUSINESS PURPOSE LOANS</h1>
+            <h2 class="text-white mx-auto mt-2 mb-5">Easy Quick Fix and Flip</h2>
+            <a class="btn btn-primary js-scroll-trigger" href="#programs">Get Started</a>
+        </div>
+    </div>
+</header>
---
+++ index.php
@@ -0,0 +1,30 @@
+<?php 
+    include('./html/header.html');
+?>
+    <body id="page-top">
+        <?php 
+            include('./components/navbar.php');
+            // Masthead
+            include('./components/header.php');
+            // Projects
+            include('./components/programs.php');
+            // About
+            include('./components/about.php');
+            // Process
+            include('./components/process.php');
+            // Contact
+            include('./components/contact.php');
+            // Footer
+            include('./components/footer.php');
+        ?>
+        <!-- Bootstrap core JS-->
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/js/bootstrap.bundle.min.js"></script>
+        <!-- Third party plugin JS-->
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery-easing/1.4.1/jquery.easing.min.js"></script>
+        <!-- Core theme JS-->
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/js/bootstrap.bundle.min.js" integrity="sha384-JEW9xMcG8R+pH31jmWH6WWP0WintQrMb4s7ZOdauHnUtxwoG2vI5DkLtS3qm9Ekf" crossorigin="anonymous"></script>
+        <script src="js/scripts.js"></script>
+
+    </body>
+</html>
