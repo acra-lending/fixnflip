873be54dd6fc0183171b1f2c87dd8193fd45b6ea
Chris <chrisliu0416@gmail.com>
2021-06-29 10:44:56 -0700

    update homepage

--- resources/views/partials/about.blade.php
+++ resources/views/partials/about.blade.php
@@ -1,9 +1,16 @@
+<style>
+    .about-section .row {
+        padding: 2.25rem 1rem;
+        border-radius: 0.25rem;
+        background-color: rgba(255, 255, 255, 0.65);
+    }
+</style>
 <section class="about-section text-center" id="about">
     <div class="container">
         <div class="row">
             <div class="col-lg-8 mx-auto">
                 <h2 class="text-black mb-4" style="font-size: 3.5rem;">S.E.T.</h2>
-                <h2 class="text-black mb-4" style="font-size 3.5rem;" id="js-rotating">-Simple-,-Efficient-,-Transparent-</h2>
+                <em><h2 class="mb-4" style="font-size 3.5rem; color: var(--primary-color);" id="js-rotating">-Simple-,-Efficient-,-Transparent-</h2></em>
                 <p class="text-black-75">
                     With over 50 years of experience as originators and C-Suite Executives, our team understands the challenges of the loan process.
                     </br>
--- resources/views/partials/programs.blade.php
+++ resources/views/partials/programs.blade.php
@@ -1,94 +1,69 @@
-<section class="projects-section " id="programs">
+<style>
+      .card {
+          height: 20rem;
+          border: none;
+          border-radius: 0;
+          /* background-color: rgb(245, 245, 245); */
+      }
+      .card img {
+          height: 70%;
+      }
+      .card-body:hover {
+        /* border: 1px solid rgba(0, 0, 0, 0.125); */
+        /* border-right: 1px solid rgba(0, 0, 0, 0.125); */
+        box-shadow: 0 0.8rem 1rem rgba(0, 0, 0, 0.15);
+        transform: translateY(-0.225rem); 
+        transition: 200ms;
+      }  
+</style>
+<section class="projects-section mb-5" id="programs">
     <div class="container-fluid">
         <!-- Project One Row-->
-        {{-- <div class="row justify-content-center no-gutters mb-5 mb-lg-0 mx-auto"> --}}
-            {{-- <div class="text-center mb-4">
-                <h2>Our Programs</h2>
-            </div> --}}
-        
-            <!-- <div class="col-lg-6"><img class="img-fluid" src="asset/img/demo-image-01.jpg" alt="" /></div> -->
-            {{-- <div class="col-lg-2 col-sm-4">
-                <div class="card border-light shadow-sm">
-                    <img class="card-img-top" src="{{ asset('img/purchase-loans-min.jpg') }}" alt="Purchase Loans">
-                        <div class="card-body">
-                            <p class="card-text text-center">Purchase Loans</p>
-                        </div>
-                </div>
-            </div>
-            <div class="col-lg-2 col-sm-4">
-                <div class="card border-light shadow-sm">
-                    <img class="card-img-top" src="{{ asset('img/purchase-rehab-min.jpg') }}" alt="Purchase Loans">
-                        <div class="card-body">
-                            <p class="card-text text-center">Purchase & Rehab
-                            </p>
-                        </div>
-                </div>
-            </div>
-            <div class="col-lg-2 col-sm-4">
-                <div class="card border-light shadow-sm">
-                    <img class="card-img-top" src="{{ asset('img/rate-term-min.jpg') }}" alt="Rate & Term Refinance">
-                        <div class="card-body">
-                            <p class="card-text text-center">Rate & Term Refinance</p>
-                        </div>
-                </div>
-            </div>
-            <div class="col-lg-2 col-sm-4">
-                <div class="card border-light shadow-sm">
-                    <img class="card-img-top" src="{{ asset('img/refinance-rehab-min.jpg') }}" alt="Refinance Rehab">
-                        <div class="card-body">
-                            <p class="card-text text-center">Refinance Rehab</p>
-                        </div>
-                </div>
-            </div>
-            <div class="col-lg-2 col-sm-4">
-                <div class="card border-light shadow-sm">
-                    <img class="card-img-top" src="{{ asset('img/cash-out-min.jpg') }}" alt="Refinance Cash Out">
-                        <div class="card-body">
-                            <p class="card-text text-center">Refinance Cash Out</p>
-                        </div>
-                </div>
-            </div>
-        </div> --}}
         <div class="card-group">
-            <div class="card" style="border-radius: 0;">
+            <div class="card">
               
-              <div class="card-body">
+              <div class="card-body justify-content-center">
+                <img src="{{ asset('img/icons/purchase_loans.svg') }}" class="card-img-top mx-auto" alt="purchase-loans">
                 <h5 class="card-title text-uppercase font-weight-bold text-center pt-3" style="color: var(--primary-color);">Purchase Loans</h4>
-                    {{-- <img src="{{ asset('img/purchase-loans-min.jpg') }}"" class="card-img-top" alt="..."> --}}
+                    
                 {{-- <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p> --}}
                 
               </div>
             </div>
-            <div class="card" style="border-radius: 0;">
+            <div class="card">
               
               <div class="card-body">
+                <img src="{{ asset('img/icons/purchase_rehab.svg') }}" class="card-img-top mx-auto" alt="purchase-rehab">
                 <h5 class="card-title text-uppercase font-weight-bold text-center pt-3" style="color: var(--primary-color);">Purchase & Rehab</h4>
-                    {{-- <img src="{{ asset('img/rate-term-min.jpg') }}" class="card-img-top" alt="..."> --}}
+
                 {{-- <p class="card-text">This card has supporting text below as a natural lead-in to additional content.</p> --}}
                 
               </div>
             </div>
-            <div class="card" style="border-radius: 0;">
+            <div class="card">
               
               <div class="card-body">
+                <img src="{{ asset('img/icons/rate_term_refinance.svg') }}" class="card-img-top mx-auto" alt="rate-term-refinance">
                 <h5 class="card-title text-uppercase font-weight-bold text-center pt-3" style="color: var(--primary-color);">Rate & Term Refinance</h4>
                     {{-- <img src="{{ asset('img/rate-term-min.jpg') }}" class="card-img-top" alt="..."> --}}
                 {{-- <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This card has even longer content than the first to show that equal height action.</p> --}}
                 
               </div>
             </div>
-            <div class="card" style="border-radius: 0;">
+            <div class="card">
               
               <div class="card-body">
+                <img src="{{ asset('img/icons/refinance_rehab.svg') }}" class="card-img-top mx-auto" alt="refinance-rehab">
                 <h5 class="card-title text-uppercase font-weight-bold text-center pt-3" style="color: var(--primary-color);">Refinance Rehab</h4>
                     {{-- <img src="{{ asset('img/refinance-rehab-min.jpg') }}" class="card-img-top" alt="..."> --}}
                 {{-- <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This card has even longer content than the first to show that equal height action.</p> --}}
                 
               </div>
             </div>
-            <div class="card" style="border-radius: 0;">
+            <div class="card">
               
               <div class="card-body">
+                <img src="{{ asset('img/icons/refinance_cash_out.svg') }}" class="card-img-top mx-auto" alt="refinance-cash-out">
                 <h5 class="card-title text-uppercase font-weight-bold text-center pt-3" style="color: var(--primary-color);">Refinance Cash Out</h4>
                     {{-- <img src="{{ asset('img/cash-out-min.jpg') }}" class="card-img-top" alt="..."> --}}
                 {{-- <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This card has even longer content than the first to show that equal height action.</p> --}}
@@ -96,20 +71,5 @@
               </div>
             </div>
           </div>
-        <!-- Project Two Row-->
-        <!-- <div class="row justify-content-center no-gutters">
-            <div class="col-lg-6"><img class="img-fluid" src="asset/img/demo-image-02.jpg" alt="" /></div>
-            <div class="col-lg-6 order-lg-first">
-                <div class="bg-black text-center h-100 project">
-                    <div class="d-flex h-100">
-                        <div class="project-text w-100 my-auto text-center text-lg-right">
-                            <h4 class="text-white">Mountains</h4>
-                            <p class="mb-0 text-white-50">Another example of a project with its respective description. These sections work well responsively as well, try this theme on a small screen!</p>
-                            <hr class="d-none d-lg-block mb-0 mr-0" />
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div> -->
     </div>
 </section>
