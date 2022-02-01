9366ea2db3f3eec6df89c0f545e3651463b21c3f
Chris <chrisliu0416@gmail.com>
2022-02-01 15:47:05 -0800

    Update URL for Ratesheet

--- resources/views/partials/ratesheets/sbmf.blade.php
+++ resources/views/partials/ratesheets/sbmf.blade.php
@@ -18,7 +18,7 @@
         <h4 class="text-center py-1 pt-2">Ratesheets</h4>
     </div>
     <div class="link">
-        <a href="https://acralending.com/wp-content/uploads/2022/RateSheets/Wholesale/acra-ff-ratematrix-sbmf.pdf" target="_blank">
+        <a href="https://acralending.com/wp-content/uploads/2022/RateSheets/FF/acra-ff-ratematrix-sbmf.pdf" target="_blank">
             <i id="icon" class="fas fa-file-pdf"></i> 
             <span id="text">Small Balance Multifamily</span>
         </a>
