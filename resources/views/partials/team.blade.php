4c62f933ec2eb7005ad3586904c17f196a3cda22
Chris <chrisliu0416@gmail.com>
2022-04-28 15:42:26 -0700

    Add Ray to Team

--- resources/views/partials/team.blade.php
+++ resources/views/partials/team.blade.php
@@ -23,11 +23,25 @@
                 </div>
                 <div class="col-lg-8">
                     <p>
-                        Mr. Jennings’ early career was serving in the U.S. Marine Corps, 0311 Infantry. He has over 30 years of experience in finance and real estate transactions. His career path has afforded him insight into an ever-changing industry, being directly involved with Fix & Flip throughout its maturity as an industry. 
+                        Mr. Jennings' early career was serving in the U.S. Marine Corps, 0311 Infantry. He has over 30 years of experience in finance and real estate transactions. His career path has afforded him insight into an ever-changing industry, being directly involved with Fix & Flip throughout its maturity as an industry. 
                         <br><br>
                         Mr. Jennings has operated in various roles from A to Z within the mortgage industry. He truly understands operations and sales, two key components to ensure successful results for investors. 
                         <br><br>    
                         Joining Acra Lending has afforded Mr. Jennings the opportunity to continue his mission of paying forward the knowledge and experience he has gained. This continues to be important in helping others to become effective in the industry.
+                    </p>
+                </div>
+            </div>
+            <div class="row mb-3 pb-3">
+                <div class="col-lg-4 text-center">
+                    <img src="{{ asset('img/raymond-riccelli.jpg') }}" class="img-fluid rounded-3 mb-3 mx-auto" alt="Rob Jennings">
+                    <p class="fw-bold text-uppercase">Raymond Riccelli</p>
+                    <p class="mb-4">Sr. Account Executive</p>
+                </div>
+                <div class="col-lg-8">
+                    <p>
+                        Mr. Riccelli has been in the Financial Industry for over 20 years and has specialized in the Real Estate Investment sector for the last 10 years. He has worked with Investors, Developers, and Builders, and has structured Financing for projects ranging from Single Family Homes to Multifamily communities with hundreds of units.
+                        <br><br>
+                        Mr. Riccelli enjoys spending time with his wife and three sons and coaches a Junior High School Basketball Team in his community.
                     </p>
                 </div>
             </div>
@@ -37,7 +51,7 @@
                     <hr class="mb-5" style="color: #0033a1; opacity: 0.75;">
                 </div>
                 <div class="col-sm-12">
-                    <p class="mb-5">With any relationship, the two key components are trust and communication. Our commitment to you is to openly communicate in a timely and effective manner, knowing that you’re trusting us as your lending partner.</p>
+                    <p class="mb-5">With any relationship, the two key components are trust and communication. Our commitment to you is to openly communicate in a timely and effective manner, knowing that you're trusting us as your lending partner.</p>
                     <a href="/apply" class="d-inline btn btn-primary mr-5">Get Pre-Approved</a>
                     <a href="/contact" class="d-inline btn btn-secondary">Contact us</a>
                 </div>
