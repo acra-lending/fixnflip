ae028505d9e4c2767d1392d325f146f54ebebebf
Chris Liu <chrisl@CSC.local>
2022-06-10 07:42:03 -0700

    Change Get Started to Get Approved in header.blade

--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -3,7 +3,7 @@
         <div class="mx-auto text-center">
             <h1 class="mx-auto mt-5 text-uppercase">PREMIER BUSINESS PURPOSE LOANS FOR REAL ESTATE INVESTORS</h1>
             <h2 class="text-white mx-auto mt-4 mb-5 pt-2">Simple Fix & Flip Solutions</h2>
-            <a class="btn btn-primary js-scroll-trigger" href="/apply">Get Started</a>
+            <a class="btn btn-primary js-scroll-trigger" href="/apply">Get Approved</a>
         </div>
     </div>
 </header>
