274bad109ccaab601b94054f2bbfb565436bd00c
Chris Liu <chrisl@CSC.local>
2022-06-20 13:43:43 -0700

    Add description to forms

--- resources/views/partials/form.blade.php
+++ resources/views/partials/form.blade.php
@@ -25,7 +25,7 @@
         <div class="accordion-item">
           <h2 class="accordion-header" id="headingTwo">
             <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseTwo" aria-expanded="false" aria-controls="collapseTwo">
-              Multi-Family Quote Request - Apply here for Multi-Family Long Term financing.
+              Multi-Family Quote Request - Apply here for Multi-Family long term financing.
             </button>
           </h2>
           <div id="collapseTwo" class="accordion-collapse collapse" aria-labelledby="headingTwo" data-bs-parent="#accordionExample">
