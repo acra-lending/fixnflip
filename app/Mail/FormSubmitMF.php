6e0894a4daab8858bc7aefc30adf47143f3b1b99
Chris Liu <chrisl@CSC.local>
2022-08-18 10:57:00 -0700

    Update email in subject for MF

--- app/Mail/FormSubmitMF.php
+++ app/Mail/FormSubmitMF.php
@@ -32,6 +32,6 @@ class FormSubmitMF extends Mailable
     public function build()
     {
         return $this->markdown('emails.form.multifam')
-                    ->subject('Fix & Flip Multi-Family Submission' .' '. $this->mail[0]['address'] .' '. $this->mail[0]['brokerEmail']);
+                    ->subject('Fix & Flip Multi-Family Submission' .' '. $this->mail[0]['address'] .' '. $this->mail[0]['email']);
     }
 }
