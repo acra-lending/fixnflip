0db7177d595e44b6fd8ae66a92983b96ed8483db
Chris Liu <chrisl@CSC.local>
2022-11-22 10:26:41 -0800

    Add columns to ratesheets page

--- resources/views/partials/ratesheets/index.blade.php
+++ resources/views/partials/ratesheets/index.blade.php
@@ -17,41 +17,47 @@
     <div class="container letter-banner">
         <h4 class="text-center py-1 pt-2">Ratesheets</h4>
     </div>
-    <div class="link">
-        <a href="https://acralending.com/wp-content/uploads/2022/RateSheets/FF/acra-ild-main.pdf" target="_blank">
-            <i id="icon" class="fas fa-file-pdf"></i> 
-            <span id="text">Main Ratesheet</span>
-        </a>
-    </div>
-    <div class="link">
-        <a href="https://acralending.com/wp-content/uploads/2022/RateSheets/FF/acra-ff-ratematrix-sbmf.pdf" target="_blank">
-            <i id="icon" class="fas fa-file-pdf"></i> 
-            <span id="text">Small Balance Multifamily</span>
-        </a>
-    </div>
-    <div class="link">
-        <a href="https://acralending.com/wp-content/uploads/2022/RateSheets/FF/acra-ild-sfr.pdf" target="_blank">
-            <i id="icon" class="fas fa-file-pdf"></i> 
-            <span id="text">SFR Bridge</span>
-        </a>
-    </div>
-    <div class="link">
-        <a href="https://acralending.com/wp-content/uploads/2022/RateSheets/FF/acra-ild-mf-longterm.pdf" target="_blank">
-            <i id="icon" class="fas fa-file-pdf"></i> 
-            <span id="text">MF Long Term</span>
-        </a>
-    </div>
-    <div class="link">
-        <a href="https://acralending.com/wp-content/uploads/2022/RateSheets/FF/acra-ild-mf-bridge.pdf" target="_blank">
-            <i id="icon" class="fas fa-file-pdf"></i> 
-            <span id="text">MF Bridge</span>
-        </a>
-    </div>
-    <div class="link">
-        <a href="https://acralending.com/wp-content/uploads/2022/RateSheets/FF/acra-ild-dscr.pdf" target="_blank">
-            <i id="icon" class="fas fa-file-pdf"></i> 
-            <span id="text">DSCR</span>
-        </a>
+    <div class="row">
+        <div class="col-6">
+            <div class="link">
+                <a href="https://acralending.com/wp-content/uploads/2022/RateSheets/FF/acra-ild-main.pdf" target="_blank">
+                    <i id="icon" class="fas fa-file-pdf"></i> 
+                    <span id="text">Main Ratesheet</span>
+                </a>
+            </div>
+            <div class="link">
+                <a href="https://acralending.com/wp-content/uploads/2022/RateSheets/FF/acra-ff-ratematrix-sbmf.pdf" target="_blank">
+                    <i id="icon" class="fas fa-file-pdf"></i> 
+                    <span id="text">Small Balance Multifamily</span>
+                </a>
+            </div>
+            <div class="link">
+                <a href="https://acralending.com/wp-content/uploads/2022/RateSheets/FF/acra-ild-sfr.pdf" target="_blank">
+                    <i id="icon" class="fas fa-file-pdf"></i> 
+                    <span id="text">SFR Bridge</span>
+                </a>
+            </div>
+        </div>
+        <div class="col-6">
+            <div class="link">
+                <a href="https://acralending.com/wp-content/uploads/2022/RateSheets/FF/acra-ild-mf-longterm.pdf" target="_blank">
+                    <i id="icon" class="fas fa-file-pdf"></i> 
+                    <span id="text">MF Long Term</span>
+                </a>
+            </div>
+            <div class="link">
+                <a href="https://acralending.com/wp-content/uploads/2022/RateSheets/FF/acra-ild-mf-bridge.pdf" target="_blank">
+                    <i id="icon" class="fas fa-file-pdf"></i> 
+                    <span id="text">MF Bridge</span>
+                </a>
+            </div>
+            <div class="link">
+                <a href="https://acralending.com/wp-content/uploads/2022/RateSheets/FF/acra-ild-dscr.pdf" target="_blank">
+                    <i id="icon" class="fas fa-file-pdf"></i> 
+                    <span id="text">DSCR</span>
+                </a>
+            </div>
+        </div>
     </div>
     <hr />
 </div>
