d68b4dabc9fdbe9d18feddb6e343cbdb531d315f
Matthew Kirby <Matthew.Kirby@CSC.local>
2023-04-17 15:38:24 -0700

    adding button to ILD

--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -4,6 +4,7 @@
             <h1 class="mx-auto mt-5 text-uppercase">PREMIER BUSINESS PURPOSE LOANS FOR REAL ESTATE INVESTORS</h1>
             <h2 class="text-white mx-auto mt-4 mb-5 pt-2">Simple Fix & Flip Solutions</h2>
             <a class="btn btn-primary js-scroll-trigger" href="/apply">Get Approved</a>
+            <a class="btn btn-primary js-scroll-trigger" href="/apply">Bank Statement Analysis</a>
         </div>
     </div>
 </header>
